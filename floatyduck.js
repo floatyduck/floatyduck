d8de5273b0cd73a56cbcdc9da2de8f27ac3ec394
Toby Sullivan <tobyjsullivan@gmail.com>
2014-02-10 12:49:30 -0800

    Fixed whitespace

--- floatyduck.js
+++ floatyduck.js
@@ -8,41 +8,41 @@ function FloatyDuck() {
   }
 
   this.Duck.prototype.moveY = function(amount) {
-  	this.y += amount;
+    this.y += amount;
   }
 
   this.Duck.prototype.moveX = function(amount) {
-  	this.x += amount;
+    this.x += amount;
   }
 
 
   this.Engine = function() {
-  	this.UPDATES_PER_SECOND = 60;
+    this.UPDATES_PER_SECOND = 60;
   }
 
   // This method updates the world (i.e., input, physics, etc)
   this.Engine.prototype.update = function() {
-  	// TODO
+    // TODO
   }
 
   // This method draws the current scene
   this.Engine.prototype.render = function() {
-  	// TODO
+    // TODO
   }
 
   this.Engine.prototype.run = function() {
-  	var updateEvery = 1000 / this.UPDATES_PER_SECOND;
-  	var lastUpdate = Date.now();
-  	while (true) {
+    var updateEvery = 1000 / this.UPDATES_PER_SECOND;
+    var lastUpdate = Date.now();
+    while (true) {
       // Update x times / second. Run multiple updates if there's been a long pause.
       var updates = (Date.now() - lastUpdate) / updateEvery;
       for(; updates > 0; updates--) {
-      	this.update();
-      	lastUpdate = Date.now();
+        this.update();
+        lastUpdate = Date.now();
       }
 
       // Rerender the screen
       this.render();
-  	}
+    }
   }
 }
