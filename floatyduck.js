797e0d64c86343491c7dc3103df04b821510181b
Toby Sullivan <tobyjsullivan@gmail.com>
2014-02-10 00:51:56 -0800

    Moved selector tool into global scope

--- floatyduck.js
+++ floatyduck.js
@@ -1,6 +1,6 @@
-function FloatyDuck() {
+$ = $ ? $ : function(a) { return document.getElementById(a); }
 
-  $ = $ ? $ : function(a) { return document.getElementById(a); }
+function FloatyDuck() {
 
   this.x = 5;
   this.y = 15;
