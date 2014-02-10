4dd0e5ec0ffe1797679bcc5cc0f7f01b99b35571
Toby Sullivan <tobyjsullivan@gmail.com>
2014-02-10 11:25:20 -0800

    Added a basic game engine

--- floatyduck.js
+++ floatyduck.js
@@ -15,4 +15,34 @@ function FloatyDuck() {
   	this.x += amount;
   }
 
+
+  this.Engine = function() {
+  	this.UPDATES_PER_SECOND = 60;
+  }
+
+  // This method updates the world (i.e., input, physics, etc)
+  this.Engine.prototype.update = function() {
+  	// TODO
+  }
+
+  // This method draws the current scene
+  this.Engine.prototype.render = function() {
+  	// TODO
+  }
+
+  this.Engine.prototype.run = function() {
+  	var updateEvery = 1000 / this.UPDATES_PER_SECOND;
+  	var lastUpdate = Date.now();
+  	while (true) {
+      // Update x times / second. Run multiple updates if there's been a long pause.
+      var updates = (Date.now() - lastUpdate) / updateEvery;
+      for(; updates > 0; updates--) {
+      	this.update();
+      	lastUpdate = Date.now();
+      }
+
+      // Rerender the screen
+      this.render();
+  	}
+  }
 }
